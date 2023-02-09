c762133237345a43f553976e1bf56a1c1133d7bb
choi <wjfekr1@naver.com>
2023-02-09 10:40:29 +0900

    Create 구현

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+function TaskBox({ text }) {
+  return <div className="box">{text}</div>;
+}
+
+function App() {
+  let [taskList, setTaskList] = useState(["리액트를 배워봅시다"]);
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    let inputValue = event.target.contents.value;
+    let newList = taskList.concat();
+    newList.push(inputValue);
+    setTaskList(newList);
+    event.target.contents.value = "";
+  };
+  return (
+    <div>
+      <div id="header">
+        <form onSubmit={onSubmitHandler}>
+          <input type="text" name="contents" />
+          <input type="submit" />
+        </form>
+        <h1>Todo List</h1>
+      </div>
+      <div id="box-area">
+        {taskList.map((one, i) => {
+          return <TaskBox key={i} text={one}></TaskBox>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
